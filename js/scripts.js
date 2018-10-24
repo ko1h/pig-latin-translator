7190bf9cf506c5d2cb0f59556f719a6b2f431778
kohichi horii and Robert Lee <robert@>
2018-10-24 14:49:28 -0700

    Add first vowell dectection

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,47 @@
-function addTwoNumbers(x,y) {asdfhslakdjf return aslkdjflasjkfl}
+function turnWordIntoArray(wordString) {
+  return wordString.split("");
+}
+function checkWordArrayForVowles(letterArray) {
+  console.log("func0: " + letterArray[0]);
+  if (letterArray[0] === "a" || letterArray[0] === "e" || letterArray[0] === "i" || letterArray[0] === "o" || letterArray[0] === "u") {
+    letterArray.push(letterArray[0], "w", "a", "y");
+    console.log("func1: " + letterArray);
+    letterArray.shift(letterArray);
+    console.log("func2: "  + letterArray);
+  } else if (letterArray[0] === "y") {
+    letterArray.push(letterArray[0], "a", "y");
+    console.log("func3: " + letterArray);
+    letterArray.shift(letterArray);
+    console.log("func4: " + letterArray);
+  }
+  return letterArray;
+}
+
+function checkForFirstVowel (letterArray) {
+  var tempWord = letterArray.join("");
+  var cutOff = tempWord.search(/.a.e.i.o.u.y/g)
+}
 
 
 $(document).ready(function() {
-  $("span#insertJavascriptOutputHere")
+  $("form#englishForm").submit(function(event) {
+    var englishSentenceArray = $("input#englishSentence").val();
+    var workingArray = turnWordIntoArray(englishSentenceArray);
+
+console.log("1st: " + workingArray);
+
+    var resultsArray = checkWordArrayForVowles(workingArray);
+
+console.log("2nd: "+ resultsArray);
+
+//LOGIC OCCURS HERE
+
+
+
+    $("span.pigLatin").text(resultsArray);
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
 });
