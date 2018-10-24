5850523f654724c51e72747fd5599970b44177ad
kohichi horii and Robert Lee <kohichi@>
2018-10-24 16:46:54 -0700

    Add final fix for translating an english word with QY

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,28 @@ function turnWordIntoArray(wordString) {
   return wordString.split("");
 }
 function checkWordArrayForVowles(letterArray) {
-  console.log("func0: " + letterArray[0]);
   if (letterArray[0] === "a" || letterArray[0] === "e" || letterArray[0] === "i" || letterArray[0] === "o" || letterArray[0] === "u") {
     letterArray.push(letterArray[0], "w", "a", "y");
-    console.log("func1: " + letterArray);
+    // console.log("func1: " + letterArray);
     letterArray.shift(letterArray);
     console.log("func2: "  + letterArray);
   } else if (letterArray[0] === "y") {
     letterArray.push(letterArray[0], "a", "y");
-    console.log("func3: " + letterArray);
+    // console.log("func3: " + letterArray);
     letterArray.shift(letterArray);
-    console.log("func4: " + letterArray);
-  } else {
+    // console.log("func4: " + letterArray);
+  }
+  // else if (-1 !== letterArray.search(/qu/g)) {
+  //   console.log("This word contains QU");
+  // }
+  else {
     letterArray.push(saveAllLettersBeforeFirstVowel(letterArray), "a", "y");
-    console.log("func3: " + letterArray);
+    // console.log("func5: " + letterArray);
     letterArray = killLettersbeforeFirstVowel(letterArray);
-    // letterArray.shift(letterArray);
-    console.log("func4: " + letterArray);
+    console.log("func6: " + letterArray);
   }
   return letterArray;
+  console.log("output of vowel check: " + letterArray);
 }
 
 function checkForFirstVowel(letterArray) {
@@ -40,25 +43,39 @@ function saveAllLettersBeforeFirstVowel(letterArray) {
   return choppedOff;
 }
 
+function checkForFirstVowelafterQU(letterArray) {
+  var tempWord = letterArray.join("");
+  var cutOff = tempWord.search(/[.a.e.i.o.y]/g)
+  return cutOff;
+}
+
+function checkForQU(letterArray) {
+  var tempWord = letterArray.join("");
+  var cutOff = tempWord.search(/[.(qu)]/g)
+  return cutOff;
+}
+
+function checkIfQuIsBeforeVowel(check4firstvowelafterq,check4qu) {
+  if (check4qu < check4firstvowelafterq) {
+    return 1
+  } else { return 0
+  }
+}
+
+
 $(document).ready(function() {
   $("form#englishForm").submit(function(event) {
     var englishSentenceArray = $("input#englishSentence").val();
     var workingArray = turnWordIntoArray(englishSentenceArray);
-console.log("check1st vowel: " + checkForFirstVowel(workingArray));
-console.log("cut off letters before 1st vowel: " + killLettersbeforeFirstVowel(workingArray));
-console.log("these were the letters before the 1st vowel: " + saveAllLettersBeforeFirstVowel(workingArray));
+    console.log("The 1st vowel is located at index: " + checkForFirstVowel(workingArray));
+    console.log("Word after slicing letters before 1st vowel: " + killLettersbeforeFirstVowel(workingArray));
+    console.log("These are the sliced letters before the 1st vowel: " + saveAllLettersBeforeFirstVowel(workingArray));
 
-console.log("1st: " + workingArray);
+    // console.log("1st: " + workingArray);
 
-    var resultsArray = checkWordArrayForVowles(workingArray);
-//now check for first vowel
-
-
-console.log("2nd: "+ resultsArray);
-
-//LOGIC OCCURS HERE
-
-
+    var resultsArray = checkWordArrayForVowles(workingArray)
+    resultsArray = resultsArray.join("");
+    console.log("2nd: "+ resultsArray);
 
     $("span.pigLatin").text(resultsArray);
 
